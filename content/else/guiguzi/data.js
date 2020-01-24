20fc4d21f47e5fb5fa110aaeaf96087cfb5b1ac1
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2020-01-24 22:28:11 +0800

    create guiguzi

---
+++ content/else/guiguzi/data.js
@@ -0,0 +1,77 @@
+var data = [
+  {
+    title: '',
+    data: [
+      {
+        title: '',
+        content: '',
+        translation: ''
+      },
+      {
+        title: '',
+        content: '',
+        translation: ''
+      }
+    ]
+  },
+  {
+    title: '',
+    data: [
+      {
+        title: '',
+        content: '',
+        translation: ''
+      },
+      {
+        title: '',
+        content: '',
+        translation: ''
+      }
+    ]
+  },
+  {
+    title: '',
+    data: [
+      {
+        title: '',
+        content: '',
+        translation: ''
+      },
+      {
+        title: '',
+        content: '',
+        translation: ''
+      }
+    ]
+  },
+  {
+    title: '',
+    data: [
+      {
+        title: '',
+        content: '',
+        translation: ''
+      },
+      {
+        title: '',
+        content: '',
+        translation: ''
+      }
+    ]
+  },
+  {
+    title: '',
+    data: [
+      {
+        title: '',
+        content: '',
+        translation: ''
+      },
+      {
+        title: '',
+        content: '',
+        translation: ''
+      }
+    ]
+  }
+];
